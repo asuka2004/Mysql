405644fd4e0608fe61b5d2489c729f95535f66de
Kung <asuka.2004@gmail.com>
2022-08-27 22:15:56 +0800

    modify install file

--- install/install_mysql.sh
+++ install/install_mysql.sh
@@ -6,13 +6,12 @@
 # System      : CentOS 7.9 
 			       
 export PS4='++ ${LINENO}'  
-export LANG=C
 export PATH=$PATH
 [ -f /etc/init.d/functions ] && . /etc/init.d/functions
 
 App_Path=/app
 [ ! -d ${App_Path} ] && mkdir -p ${App_Path}
-Soft_Path=/tool/soft
+Soft_Path=/tool
 [ ! -d ${Soft_Path} ] && mkdir -p ${Soft_Path}
 
 Check_User(){
@@ -23,29 +22,40 @@ Check_User(){
 	fi	
 }
 
-Install_Mysql(){
+Setup_User(){
+	groupadd mysql
 	useradd -s /sbin/nologin -g mysql -M mysql
-	cd ${Soft_Path}
-	wget https://downloads.mysql.com/archives/get/p/23/file/mysql-5.7.37-el7-x86_64.tar.gz
-	tar -zxvf  mysql-5.7.37-el7-x86_64.tar.gz 
-	mv mysql-5.7.37 ${App_Path}
-	ln -s /app/mysql-5.7.37  ${App_Path}/mysql
 	chown -R mysql:mysql ${App_Path}/mysql
-	${App_Path}/mysql/bin/mysqld --initialize-ubsecure --user=mysql --basedir=${App_Path}/mysql --datadir=${App_Path}/mysql/data
 }
 
 Setup_Config(){
-	echo "datadir=/app/mysql/data" >> /etc/mysql.cnf
-	echo "socket=/var/lib/mysql/mysql.sock">>/etc/mysql.cnf
-	echo "server_id=1">>/etc/mysql.cnf
-	echo "log_error=/var/log/mysqld.log">>/etc/mysql.cnf
-	echo "port=3306">>/etc/mysql.cnf
-	echo "export PATH=/app/mysql/bin:$PATH">>/etc/profile
+	echo "basedir=${App_Path}/mysql" >> /etc/my.cnf
+	echo "datadir=${App_Path}/mysql/data" >> /etc/my.cnf
+	echo "socket=/var/lib/mysql/mysql.sock">>/etc/my.cnf
+	echo "server_id=1">>/etc/my.cnf
+	echo "log_error=${App_Path}/mysql/data/mysql.err">>/etc/my.cnf
+	echo "port=3306">>/etc/my.cnf
+	echo "export PATH=${App_Path}/mysql/bin:$PATH">>/etc/profile
 	source /etc/profile
 }
 
-Setup_systemctl(){
-	cp ${Soft_Path}/mysqld.service /etc/systemd/system/mysqld.service
+Install_Mysql(){
+	cd ${Soft_Path}/software
+	#wget https://downloads.mysql.com/archives/get/p/23/file/mysql-5.7.37-el7-x86_64.tar.gz
+	tar -zxvf  mysql-5.7.37-el7-x86_64.tar.gz 
+	mv mysql-5.7.37-el7-x86_64 ${App_Path}/mysql-5.7.37
+	ln -s ${App_Path}/mysql-5.7.37  ${App_Path}/mysql
+	${App_Path}/mysql/bin/mysqld --initialize-insecure --user=mysql --basedir=${App_Path}/mysql --datadir=${App_Path}/mysql/data
+	if [ $? -eq 0 ] 
+         then
+                 action "Success to install " /bin/true
+        else
+                 action "Fail to install" /bin/false
+        fi  
+}
+
+Setup_daemon(){
+	cp ${Soft_Path}/script/mysqld.service /etc/systemd/system/mysqld.service
 	systemctl daemon-reload
   	systemctl enable mysqld
 	systemctl start mysqld
@@ -53,8 +63,9 @@ Setup_systemctl(){
 
 main(){
 	Check_User
-	Install_Mysql	
-	Setup_Config
-	Setup_systemctl
+	Setup_User
+	Install_Mysql
+	Setup_Config	
+#	Setup_daemon
 }
 main 
