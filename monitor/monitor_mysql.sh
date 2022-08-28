30cce58b5a20ffa290c4ef361a1a58bfb46d2a03
Kung <asuka.2004@gmail.com>
2022-08-28 21:07:27 +0800

    modify monitor file

--- monitor/monitor_mysql.sh
+++ monitor/monitor_mysql.sh
@@ -10,11 +10,26 @@ export LANG=C
 export PATH=$PATH
 [ -f /etc/init.d/functions ] && . /etc/init.d/functions
 
+App_Path=/app
+[ ! -d ${App_Path} ] && mkdir -p ${App_Path}
+Soft_Path=/tool
+[ ! -d ${Soft_Path} ] && mkdir -p ${Soft_Path}
+
+File_Path=/var/spool/cron/root
+
+if [ `grep "monitor_mysql" ${File_Path}|wc -l` -lt 1 ]
+ then
+	echo "Monitor DB" >>/var/spool/cron/root
+	echo "*/5 * * * * /bin/sh  ${Soft_Path}/script/monitor_mysql.sh" >>/var/spool/cron/root
+else
+	echo "Nothing ">/dev/null 2>&1		
+fi
+
+
 if [ `netstat -ntulp|grep mysqld|wc -l` -gt 0 ]
  then
 	echo  "Mysql is OK"  >/dev/null 2>&1
 else
-	Critical="Waining!!!! Mysql is down"
+	Critical="Warning!!!! Mysql is down"
 	echo $Critical | tee | mail -s "$Critical" root@localhost
-	exit 2
 fi
